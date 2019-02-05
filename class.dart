cd2d51b812c291a1fde21f5cadf6abf85b30292a
TharuSithumini <tharusithumini@gmail.com>
2019-02-05 10:40:42 +0530

    dart4

---
+++ class.dart
@@ -0,0 +1,35 @@
+void main() {
+  
+  var studentx=student();
+  studentx.age;
+  studentx.name="jone";
+  print("${studentx.age}  ${studentx.name} ");
+  studentx.findAge();
+  studentx.stdname();
+  
+}
+
+
+
+class student{
+  
+  int age;
+  String name;
+ 
+
+
+void findAge(){
+  
+  
+  
+}
+
+
+void stdname(){
+  
+  
+  
+}
+
+
+}
